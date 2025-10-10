b4f45702654af04b85f494935662c15f95a916f6
Tanzin-Ul-Islam <tanzin.8897@gmail.com>
2025-10-10 23:00:36 +0600

    HttpException filter removed

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
-import { AllExceptionFilter } from './HttpExceptionFilter';
 import { UsersModule } from './users/users.module';
 import { CoreModule } from './core/core.module';
 import { AuthModule } from './auth/auth.module';
@@ -34,12 +33,6 @@ import { BillingAddressModule } from './address/billing-address/billing-address.
     BillingAddressModule,
   ],
   controllers: [AppController],
-  providers: [
-    AppService,
-    {
-      provide: 'APP_FILTER',
-      useClass: AllExceptionFilter,
-    },
-  ],
+  providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
